bc7069293c3d5eaf44fec9e6f0616b171c965253
PowerSpear45 <vssvchn@gmail.com>
2026-07-03 23:46:57 +0530

    Remove duplicate repositories and add fees module

--- backend/src/main/java/com/schoolapp/entity/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.schoolapp.repository;
-
-import com.schoolapp.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-    long countByStatus(String status);
-}
--- backend/src/main/java/com/schoolapp/entity/TeacherRepository.java
+++
@@ -1,8 +0,0 @@
-package com.schoolapp.repository;
-
-import com.schoolapp.entity.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
-    long countByStatus(String status);
-}
