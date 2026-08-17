2f9980afde9495ecd8523b1d1a3ad12ee430b845
PowerSpear45 <vssvchn@gmail.com>
2026-08-17 16:25:33 +0530

    Fix class teacher ID type

--- backend/src/main/java/com/schoolapp/entity/ClassSection.java
+++ backend/src/main/java/com/schoolapp/entity/ClassSection.java
@@ -20,45 +20,43 @@ public class ClassSection {
     @Column(name = "class_teacher")
     private Integer classTeacher;
 
-
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getClassName() {
         return className;
     }
 
-    public String getSection() {
-        return section;
-    }
-
-    public Integer getStrength() {
-        return strength;
-    }
-
-    public String getClassTeacher() {
-        return classTeacher;
-    }
-
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public void setClassName(String className) {
         this.className = className;
+    }
+
+    public String getSection() {
+        return section;
     }
 
     public void setSection(String section) {
         this.section = section;
     }
 
+    public Integer getStrength() {
+        return strength;
+    }
+
     public void setStrength(Integer strength) {
         this.strength = strength;
     }
 
-    public void setClassTeacher(String classTeacher) {
+    public Integer getClassTeacher() {
+        return classTeacher;
+    }
+
+    public void setClassTeacher(Integer classTeacher) {
         this.classTeacher = classTeacher;
     }
 }
